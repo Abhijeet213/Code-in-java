dd6b7f29686a5707e0093519713a64ce1e4e0647
Abhijeetbiss <108187308+Abhijeet213@users.noreply.github.com>
2023-02-05 11:49:38 +0530

    Update LL.java

--- LL.java
+++ LL.java
@@ -1,56 +1,84 @@
 package Linkedlist;
 
+// There is Linked List Heap
 public class LL {
-    private   Node head; 
-    private   Node tail;
-    private   int size =0; 
+    private Node head; // Here are the First Node of linked list chain
+    private Node tail; // Here are the Last Node of linked list chain
+    private int size = 0; // Here are the Size of linked list chain
 
-private  class Node {
-    int value;
-   Node nextNode;
-    public Node(int value) {
-        this.value = value;
+    private class Node { // Here Are The Structrure of Node 
+        int value;       // Value of Single Node
+
+        Node nextNode;   // Here Stores The next node Address
+
+        public Node(int value) {// Here are A constructor for seting Value With initialization
+            this.value = value;
+        }
+
+        public Node() { // Here are A null constructor 
+        }
+
     }
-    public Node() {
+
+    public void Print() {
+        Node temp = head;
+        for (int i = 0; i < size; i++) {
+            System.out.println(temp.value);
+
+            temp = temp.nextNode;
+        }
+
     }
-    
+ 
+public void helper (){
+    Node Head = head;
+    reversethelinkedlist(Head);
 }
+    public void reversethelinkedlist(Node Head) {
+      
+        if (Head ==null){
+            return;
+        }
+       
+        reversethelinkedlist(Head.nextNode);
+        if (Head ==null){
+            return;
+        }
+        System.out.println(Head.value);
 
-public  void Print() {
-    Node temp=head;
-    for (int i = 0; i < size; i++) {
-        System.out.println(temp.value);
+    } 
+    public void addLast(int val) {
+        Node newnode = new Node(val);
+        tail.nextNode = newnode;
+        size++;
+    }
+int i =0;
+    public void ADDFIRST(int val) {
+        if (i ==1 ){
+            tail=head;
+            i++;
+        }
+        Node newnode = new Node(val);
+        newnode.nextNode = head;
         
-        temp= temp.nextNode;
+        head = newnode;
+        size++;
+i++;
     }
-    
-}
-public  void addLast(int val) {
-Node newnode = new Node(val);
-tail.nextNode=newnode;
-size++;
-}
-public  void ADDFIRST(int val) {
-    Node newnode = new Node(val);
-     newnode.nextNode=head;
-    head=newnode;
-size++;
 
-    }
-    public  void add(int index, int element){
+    public void add(int index, int element) {
         Node newnode = new Node(element);
-        Node temp =head;
-        for (int i = 1; i <= index-1; i++) {
-            if (temp.nextNode ==null){
-            System.out.println("This Index Not Exists In LINKED LIST");
-            }
-            else{
-                temp=temp.nextNode;
+        Node temp = head;
+        for (int i = 1; i <= index - 1; i++) {
+            if (temp.nextNode == null) {
+                System.out.println("This Index Not Exists In LINKED LIST");
+            } else {
+                temp = temp.nextNode;
             }
         }
         Node temp1 = temp.nextNode;
-        temp.nextNode=newnode;
-        newnode.nextNode= temp1;
+        temp.nextNode = newnode;
+        newnode.nextNode = temp1;
 
         size++;
 
